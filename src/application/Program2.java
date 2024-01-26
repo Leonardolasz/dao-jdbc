cb83a7a569336de5efec29dbe51e143d25f99c8a
leonardo alves <109185275+Leonardolasz@users.noreply.github.com>
2024-01-26 18:12:46 -0300

    Department delete implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -40,6 +40,12 @@ public class Program2 {
         System.out.println();
         System.out.println("Update completed: " + dep.getName());
 
+        System.out.println("\n===TEST 5: department delete ===");
+        System.out.println();
+        System.out.print("Enter id for delete test: ");
+        int id = sc.nextInt();
+        departmentDao.deleteById(id);
+        System.out.println("Delete completed!");
 
         sc.close();
     }
